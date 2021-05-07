d47c9106326d03b99e78cd4ba6a1261b7e73264d
Oscar Rodriguez Mira <rodriguezmiraoscar@gmail.com>
2021-05-07 17:53:51 +0200

    Input checks when submit and messages for errors

--- src/index.js
+++ src/index.js
@@ -3,70 +3,78 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { provincias } from './data/provincias';
 import { producto } from './data/producto';
-import { cliente } from './data/cliente';
+import { cliente } from './data/cliente';  
 
     class FormCheck extends React.Component{
         constructor(props){
-            super(props);
-            const [name, setName] = useState(cliente.name);
-            const [surn, setSurn] = useState(cliente.surname);
-            const [emai, setEmai] = useState(cliente.email);
-            const [phone, setPhone] = useState('');
-            const [address, setAddress] = useState('');
-            const [adnum, setAdnum] = useState('');
-            this.state={              
+            super(props)       
+
+            this.state={
+                display:false,      
+                name:'',
+                surname:'',
+                phone:0,
+                address:'',
+                naddress:0,
                 selectvalue: 'Alicante'              
             };
             
-            this.handleSubmit = this.handleSubmit.bind(this);
             this.select_handleChange = this.select_handleChange.bind(this);
-        }
+        } 
 
-        
+        inputCheck = (e) => {
+            let filter = e.target.getAttribute('filter')   
+            e.target.value = e.target.value.replace(new RegExp(filter, 'g'), '')   
+            this.setState({[e.target.name]: e.target.value})          
+          }
 
-        handleSubmit(event){
-            if(this.state.nomvalue===''||this.state.apellvalue===''||this.state.emavalue===''||this.state.telvalue===''||this.state.dirvalue===''||this.state.dnumvalue===''){
-                console.log('Nada, jaja, polla')
-                //cliente.name = document.getElementById('nom').value;
-                console.log();
+        handleSubmitCheck = () => {
+            if(!this.state.name || !this.state.surname){
+                alert("Introduce tu nombre y apellidos");
+            }else if(!this.state.email.match(/@./g)){
+                alert("Introduce el email correctamente");
+            }else if(this.state.phone.length<9 || !this.state.phone){
+                alert("Introduce un numero de teléfono");
+            }else if(!this.state.address){
+                alert("Introduce una dirección válida");
+            }else if(this.state.naddress.length<3||!this.state.naddress){
+                alert("Introduce un número de dirección válido");
             }else{
-                cliente.name = document.getElementById('nom').value;
-                cliente.surname = document.getElementById('apell').value;
-                cliente.ema = document.getElementById('ema').value;
-                cliente.phone = document.getElementById('tel').value;
-                cliente.address = document.getElementById('dir').value;
-                cliente.addressNumber = document.getElementById('dnum').value;
-                cliente.province = this.state.selectvalue
-                console.log(this.state.nomvalue);
+                this.setState=({display:true})
             }
         }
 
         select_handleChange(event){
             this.setState({ selectvalue: event.target.selectvalue });            
         }
-
-        name_handleChange(event){
-            this.setState({  });
-        }
-
         
-
+        reset_form = () =>{
+            this.setState({
+                display: !this.state.display,
+                name:'',
+                surname:'',
+                phone:0,
+                address:'',
+                naddress:0,
+                selectvalue: 'Alicante'
+            });
+        }
 
         render(){
             return(
-                <form onSubmit={this.handleSubmit}>
+                <div className="form">
                     <label className="form-label">Nombre</label>
-                    <input id='nom' name="nom" type="text" className="form-control" />
+                    <input filter="[^a-zA-Z ]" id='nom' name="nom" type="text" onChange={this.inputCheck} className="form-control" />
                     <label className="form-label">Apellidos</label>
-                    <input id='apell' name="apell" type="text" className="form-control"/>
+                    <input filter="[^a-zA-Z ]" id='apell' name="apell" type="text" onChange={this.inputCheck} className="form-control"/>
                     <label className="form-label">Email</label>
                     <input name="ema" type="email" className="form-control"/>
                     <label className="form-label">Teléfono</label>
-                    <input name="tel" type="text" className="form-control"/>
+                    <input filter="[^0-9]" maxLength="9" name="tel" type="text" onChange={this.inputCheck} className="form-control"/>
                     <label className="form-label">Dirección</label>
                     <input name="dir" type="text" className="form-control"/>
                     <label className="form-label">Número de dirección</label>
-                    <input  name="dnum" type="text" maxLength="3" className="form-control"/>
+                    <input filter="[^0-9]" name="dnum" type="text" maxLength="3" onChange={this.inputCheck} className="form-control"/>
                     <label className="form-label">Provincias</label>
                     <select value={this.state.selectvalue} onChange={this.select_handleChange} className="form-select">
                     {provincias.map((provincia) => (
@@ -74,10 +82,10 @@ import { cliente } from './data/cliente';
                             {provincia}
                         </option>
                     ))}
-                </select>
-                <br/>
-                    <input className="btn btn-primary" type="submit"/>
-                </form>
+                    </select>
+                    <br/>
+                    <button className="btn btn-primary">Enviar</button>
+                </div> 
             );            
         }
     }
